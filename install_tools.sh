dfee2863c3177379d0748c26c162bdb0ce566778
PaDuMo <pdurmol005@gmail.com>
2025-10-01 08:37:43 +0200

    install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source script.env
+
+echo "Instalando phpMyAdmin..."
+sudo apt install phpmyadmin -y
+
+echo "Configurando contraseña de phpMyAdmin...
+
+echo "Instalando AWStats..."
+sudo apt install awstats -y
+
+echo "Herramientas adicionales instaladas."
